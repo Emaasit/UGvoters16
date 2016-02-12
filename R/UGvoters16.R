0da3c0f3bc664a1ff2929cff32ff2b3bbec00973
Emaasit <daniel.emaasit@gmail.com>
2016-02-12 01:03:09 -0800

    version 0.1.0

---
+++ R/UGvoters16.R
@@ -0,0 +1,24 @@
+#' UGvoters16.
+#'
+#' Counts of Ugandan registered voters for the 2016 General Election
+#'
+#' @format A data frame with 28010 observations and 14 variables:
+#' \describe{
+#'   \item{ser_no}{serial number}
+#'   \item{dist_code}{district code}
+#'   \item{district_name}{district name}
+#'   \item{ea_code}{EA code}
+#'   \item{ea_name}{EA name}
+#'   \item{scty_code}{SCounty code}
+#'   \item{scounty_name}{SCounty name}
+#'   \item{par_code}{Parish code}
+#'   \item{parish_name}{Parish name}
+#'   \item{ps_code}{Polling Station code}
+#'   \item{ps_name}{Polling Station name}
+#'   \item{no_of_females}{Number of females}
+#'   \item{no_of_males}{Number of males}
+#'   \item{ec_voter_counts}{Voter counts by the Electoral Commission}
+#'
+#' }
+#' @source \url{http://www.ec.or.ug/}
+"UGvoters16"
---
+++ R/analyzed.R
@@ -0,0 +1,25 @@
+#' Analyzed voter counts by Media.
+#'
+#' Analyzed Counts of Ugandan registered voters for the 2016 General Election
+#'
+#' @format A data frame with 28010 observations and 15 variables:
+#' \describe{
+#'   \item{ser_no}{serial number}
+#'   \item{dist_code}{district code}
+#'   \item{district_name}{district name}
+#'   \item{ea_code}{EA code}
+#'   \item{ea_name}{EA name}
+#'   \item{scty_code}{SCounty code}
+#'   \item{scounty_name}{SCounty name}
+#'   \item{par_code}{Parish code}
+#'   \item{parish_name}{Parish name}
+#'   \item{ps_code}{Polling Station code}
+#'   \item{ps_name}{Polling Station name}
+#'   \item{no_of_females}{Number of females}
+#'   \item{no_of_males}{Number of males}
+#'   \item{ec_voter_counts}{Voter counts by the Electoral Commission}
+#'   \item{analyzed_voter_counts}{Analyzed Voter Counts by Media}
+#'
+#' }
+#' @source \url{https://jssozi.wordpress.com/2016/02/09/at-least-20000-ghost-voters-in-ugandas-voter-register-9-days-to-elections/}
+"analyzed"
