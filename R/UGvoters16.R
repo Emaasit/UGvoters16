507cd73efc75a626ba78f85b29eb20a918c6f936
Emaasit <daniel.emaasit@gmail.com>
2016-02-13 13:30:56 -0800

    corrected variable names

--- R/UGvoters16.R
+++ R/UGvoters16.R
@@ -4,20 +4,20 @@
 #'
 #' @format A data frame with 28010 observations and 14 variables:
 #' \describe{
-#'   \item{ser_no}{serial number}
-#'   \item{dist_code}{district code}
-#'   \item{district_name}{district name}
-#'   \item{ea_code}{EA code}
-#'   \item{ea_name}{EA name}
-#'   \item{scty_code}{SCounty code}
-#'   \item{scounty_name}{SCounty name}
-#'   \item{par_code}{Parish code}
-#'   \item{parish_name}{Parish name}
-#'   \item{ps_code}{Polling Station code}
-#'   \item{ps_name}{Polling Station name}
-#'   \item{no_of_females}{Number of females}
-#'   \item{no_of_males}{Number of males}
-#'   \item{ec_voter_counts}{Voter counts by the Electoral Commission}
+#'   \item{SER_NO}{serial number}
+#'   \item{DIST_CODE}{district code}
+#'   \item{DISTRICT_NAME}{district name}
+#'   \item{EA_CODE}{EA code}
+#'   \item{EA_NAME}{EA name}
+#'   \item{SCTY_CODE}{SCounty code}
+#'   \item{SCOUNTY_NAME}{SCounty name}
+#'   \item{PAR_CODE}{Parish code}
+#'   \item{PARISH_NAME}{Parish name}
+#'   \item{PS_CODE}{Polling Station code}
+#'   \item{PS_NAME}{Polling Station name}
+#'   \item{NO_OF_FEMALES}{Number of females}
+#'   \item{NO_OF_MALES}{Number of males}
+#'   \item{EC_VOTER_COUNTS}{Voter counts by the Electoral Commission}
 #'
 #' }
 #' @source \url{http://www.ec.or.ug/}
